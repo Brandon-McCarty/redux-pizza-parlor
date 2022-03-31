b54bc9f32c226525fc06cddd911b28db388f8aeb
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-03-31 13:40:25 -0500

    PizzaItem component; add: conditional render for delete and add button

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,29 +1,45 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
+import {useState} from 'react';
 
 // Displays a single Product with price on the DOM
 function PizzaItem({ getPizzas, pizzaItem }) {
 
-    const deletePizza = () => {
-
+    const [buttonOption, setButtonOption] = useState(true);
 
     const dispatch = useDispatch();
 
+// dispatch({type: 'ADD_TO_CART', payload: {name: pizzaItem.name, description: pizzaItem.description, price: pizzaItem.price, image: pizzaItem.image}})
+// dispatch({type: 'ADD_TO_COST', payload: product.price})
+
+    const toggleAddRemove = () => {
+
+      
+    }
+
+    console.log(buttonOption);
+
+    const deletePizza = () => {
+
+
+    }
+
     const addPizzaToCart = () => {
         console.log(pizzaItem);
-    
-    // dispatch({type: 'ADD_TO_CART', payload: {name: pizzaItem.name, description: pizzaItem.description, price: pizzaItem.price, image: pizzaItem.image}})
-    // dispatch({type: 'ADD_TO_COST', payload: product.price})
+
+        setButtonOption(!buttonOption)
     }
 
     return (
+        <>
         <li>
             {pizzaItem.name}: {pizzaItem.price}
-            <button onClick={addPizzaToCart}>Add</button>
-            <button onClick={deletePizza}>Delete</button>
+            {buttonOption ? <button onClick={addPizzaToCart}>Add</button> : <button onClick={deletePizza}>Delete</button>}
         </li>
+        </>
     );
 }
+
 
 export default PizzaItem;
