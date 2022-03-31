ccef19e3ab78e81e58edf2a1aff7c5437c60344a
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-31 13:14:07 -0500

    save point to merge main

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+
 
 function CustomerForm({ getPizzas }) {
 
@@ -7,46 +7,73 @@ function CustomerForm({ getPizzas }) {
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
     const [zip, setZip] = useState('');
+    const [orderType, setOrderType] = useState('Delivery'); // Need to grab from radio input
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(name, address, city, zip, orderType);
+        setName('');
+        setAddress('');
+        setCity('');
+        setZip('');
+        setOrderType('');
+    }
+
 
     return (
         <>
-            <input
-                required
-                placeholder="Name"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-            />
-            <br />
-            <input
-                required
-                placeholder="Street Address"
-                value={address}
-                onChange={(event) => setAddress(event.target.value)}
-            />
-            <br />
-            <input
-                required
-                placeholder="City"
-                value={city}
-                onChange={(event) => setCity(event.target.value)}
-            />
-            <br />
-            <input
-                required
-                placeholder="Zip"
-                value={zip}
-                onChange={(event) => setZip(event.target.value)}
-            />
-            <br />
-            
-            <input type="radio" id="pickup" value="Pickup"/>
-            <label htmlFor="pickup">Pickup</label>
-            
-            <input type="radio" id="delivery" value="Delivery"/>
-            <label htmlFor="pickup">Delivery</label>
-            <button>
-                Next
-            </button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    required
+                    placeholder="Name"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
+                <br />
+                <input
+                    required
+                    placeholder="Street Address"
+                    value={address}
+                    onChange={(event) => setAddress(event.target.value)}
+                />
+                <br />
+                <input
+                    required
+                    placeholder="City"
+                    value={city}
+                    onChange={(event) => setCity(event.target.value)}
+                />
+                <br />
+                <input
+                    type="number"
+                    required
+                    placeholder="Zip"
+                    value={zip}
+                    onChange={(event) => setZip(event.target.value)}
+                />
+                <br />
+
+                <input type="radio" 
+                name ="chooseDelivery" 
+                id="pickup" 
+                value="Pickup" 
+                onChange={handleChange}
+                />
+                <label htmlFor="pickup">Pickup</label>
+
+                <input type="radio" 
+                name ="chooseDelivery" 
+                id="delivery" 
+                value="Delivery" 
+                
+                />
+                <label htmlFor="pickup">Delivery</label>
+                <br />
+                <button type="submit">
+                    Next
+                </button>
+            </form>
         </>
     )
 }
