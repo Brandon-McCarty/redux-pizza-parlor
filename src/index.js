8d02f6cb9dbbc7ae9aa31b244196ce57dd20d554
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-03-31 16:45:53 -0500

    Index.js: add: shoppingCart Reducer, fix: targeted id in return. PizzaItem.jsx; add: dispatch and inc id in payload.

--- src/index.js
+++ src/index.js
@@ -4,39 +4,54 @@ import './index.css';
 import App from './components/App/App';
 
 //REDUX Imports
-import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 // REDUCERS
-const pizzaList = ( state = [], action ) => {
-    if( action.type === 'SET_PIZZA_LIST'){
+const pizzaList = (state = [], action) => {
+    if (action.type === 'SET_PIZZA_LIST') {
         return action.payload
     }
     return state;
-}
+};
 
 const customerInformation = (state = {}, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'ADD_CUSTOMER_INFORMATION':
             return action.payload
         default:
             return state
     }
+};
 
-}
+const shoppingCart = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TO_CART':
+            return [...state, action.payload]
+        case 'DELETE_FROM_CART':
+            console.log(action.payload.id);
+            
+            return state.filter((pizzaItem) => pizzaItem.id !== action.payload.id)
+        case 'CLEAR_CART':
+            return [];
+        default:
+            return state;
+    }
+};
 
 const reduxStore = createStore(
     combineReducers({
-      pizzaList,
-      customerInformation
+        pizzaList,
+        customerInformation,
+        shoppingCart
     }),
     applyMiddleware(logger)
-  );
+);
 
 ReactDOM.render(
     <Provider store={reduxStore}>
-            <App /> 
+        <App />
     </Provider>,
     document.getElementById('root')
 );
